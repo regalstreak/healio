0690c9fceaddd9df59d736e94959a97ee14072e6
Neil Agarwal <neil@neilagarwal.me>
2019-10-05 20:44:20 +0530

    healio: Move home to home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Sidebar from './component/Sidebar';
 import ResearchList from './component/ResearchList';
 import { Switch, Route } from 'react-router-dom';
+import Home from './screens/Home';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Sidebar />
+      {/* <Sidebar />
       <Switch>
         <Route path='/app' component={ResearchList} />
-      </Switch>
+      </Switch> */}
+      <Home />
     </div>
   );
 }
