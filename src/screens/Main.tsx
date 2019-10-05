135c0b8c84c3424de0eca9ea05b4ce0fb9c409b8
Neil Agarwal <neil@neilagarwal.me>
2019-10-05 17:35:31 +0530

    healio: Directory structure and react-router

---
+++ src/screens/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface INeilProps {
+
+}
+
+export default (props: INeilProps) => {
+    return (
+        <div>
+            <div>Hello Neil</div>
+        </div>
+    )
+}
